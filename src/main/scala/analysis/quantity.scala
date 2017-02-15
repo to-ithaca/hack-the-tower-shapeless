0908a1a6ba82a19de1a929047be4a9ef8d9ba14f
zainab-ali <zainab.ali.london@gmail.com>
2017-02-15 17:48:01 +0000

    Step 01 (#6)

--- src/main/scala/analysis/quantity.scala
+++ src/main/scala/analysis/quantity.scala
@@ -2,8 +2,3 @@ package analysis
 
 //TODO: make this parameterized on any type
 case class Quantity[I <: Singleton with Int, D <: Dimensions[_, _]](value: Double)
-
-object Quantity {
-  import analysis.syntax.QuantityOps
-  implicit def quantityOps[P <: Singleton with Int, D <: Dimensions[_, _]](q: Quantity[P, D]): QuantityOps[P, D] = new QuantityOps(q)
-}
